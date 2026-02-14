07db4464c724b81adf957af9d597465e8fb79a63
germanHernando <hernandoger@hotmail.com>
2026-02-14 11:56:43 -0300

    corrigiendo sentencias sql, creando tablas e inicializando enums

--- src/main/resources/database/create_db.sql
+++ src/main/resources/database/create_db.sql
@@ -2,29 +2,30 @@ create table USUARIOS(
 ID bigint not null AUTO_INCREMENT,
 EMAIL varchar(70) not null,
 CONTRASENIA varchar(70) not null,
-PRIMARY KEY(ID);
+PRIMARY KEY(ID)
 );
 
 CREATE TABLE COMPRADORES(
 ID bigint not null,
 NOMBRE varchar(60)not null,
 APELLIDO varchar(60)not null,
-TELEFONO int not null,
+TELEFONO varchar(20) not null,
 DIRECCION varchar(90)not null,
-CODIGO_POSTAL int not null,
-PRIMARY KEY(ID);
+CODIGO_POSTAL varchar(10) not null,
+PRIMARY KEY(ID)
 );
 alter table COMPRADORES add constraint COMP$USU foreign key(ID) references USUARIOS(ID);
 
 
 CREATE TABLE PEDIDOS(
 ID bigint not null AUTO_INCREMENT,
+COMPRADOR_ID bigint not null,
 FECHA timestamp not null,
 TOTAL decimal(9,2)not null,
 ESTADO varchar(50)not null,
-PRIMARY KEY(ID);
+PRIMARY KEY(ID)
 );
-alter table PEDIDOS add constraint PED$COMP foreign key(COMPRADOR_ID) references COMPRADORES(ID)
+alter table PEDIDOS add constraint PED$COMP foreign key(COMPRADOR_ID) references COMPRADORES(ID);
 
 CREATE TABLE PRODUCTOS(
 ID bigint not null AUTO_INCREMENT,
@@ -33,24 +34,24 @@ MARCA varchar(70)not null,
 COLOR varchar(50)not null,
 PRECIO decimal(9,2) not null,
 CANTIDAD int not null,
-PRIMARY KEY(ID);
+PRIMARY KEY(ID)
 );
 
 CREATE TABLE CELULARES(
 ID bigint not null,
 TAMANIO int not null, 
 RESOLUCION_PANTALLA varchar(60)not null,
-RESULOCION_CAMARA varchar(60)not null,
+RESOLUCION_CAMARA varchar(60)not null,
 RAM int not null,
 ALMACENAMIENTO int not null,
-PRIMARY KEY(ID);
+PRIMARY KEY(ID)
 );
 
 CREATE TABLE CARGADORES(
 ID bigint not null,
 WATS int not null,
 CARGA_RAPIDA boolean not null,
-PRIMARY KEY(ID);
+PRIMARY KEY(ID)
 );
 
 alter table CELULARES add constraint CEL$PRO foreign key(ID) references PRODUCTOS(ID);
@@ -62,10 +63,10 @@ PEDIDO_ID bigint not null,
 PRODUCTO_ID bigint not null,
 CANTIDAD int not null,
 PRECIO_UNITARIO decimal(9,2) not null,
-PRIMARY KEY(ID);
+PRIMARY KEY(ID)
 );
- alter table ITEMS_PEDIDOS add constraint ITEM$PED foreign key(PEDIDO_ID) references PEDIDOS(ID),
- alter table ITEMS_PEDIDOS add constraint ITEM$PRO foreign key(PRODUCTO_ID) references PRODUCTOS(ID)
+ alter table ITEMS_PEDIDOS add constraint ITEM$PED foreign key(PEDIDO_ID) references PEDIDOS(ID);
+ alter table ITEMS_PEDIDOS add constraint ITEM$PRO foreign key(PRODUCTO_ID) references PRODUCTOS(ID);
 
 CREATE TABLE PAGOS (
 ID bigint not null AUTO_INCREMENT,
@@ -74,19 +75,19 @@ FECHA_PAGO timestamp not null,
 METODO_PAGO varchar(30) not null,
 MONTO decimal(9,2) not null,
 ESTADO varchar(50)not null,
-PRIMARY KEY(ID);
+PRIMARY KEY(ID)
 );
-alter table PAGOS add constraint PAGO$PED foreign key(PEDIDO_ID) references PEDIDOS(ID)
+alter table PAGOS add constraint PAGO$PED foreign key(PEDIDO_ID) references PEDIDOS(ID);
     
 CREATE TABLE ESTADOS(
 ID int not null,
 VALOR varchar(50)not null,
-PRIMARY KEY(ID);
-)
+PRIMARY KEY(ID)
+);
     
 CREATE TABLE METODOS_PAGOS(
 ID int not null,
 VALOR varchar(50)not null,
-PRIMARY KEY(ID);
-)
-    
+PRIMARY KEY(ID)
+);
+    
