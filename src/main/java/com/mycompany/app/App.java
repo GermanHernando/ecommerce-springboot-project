7c0c57aad881cc6b577c90cefd2f639f9b48c7b0
germanHernando <hernandoger@hotmail.com>
2026-03-07 10:01:12 -0300

    Se agregó dependencia springboot-starter-web, runner en App.java y se creó modelo starter de html y css

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,6 @@
 package com.mycompany.app;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,7 +11,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = {"models"})
 @EnableJpaRepositories(basePackages = {"repositories"})
 public class App{
+	
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    	SpringApplication.run(App.class, args);
     }
 }
