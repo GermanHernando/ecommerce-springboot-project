71c436cc688018b546da5685abf73bac42dccd3a
germanHernando <hernandoger@hotmail.com>
2026-04-05 19:47:45 -0300

    Nuevo package template.unsecured, se movieron clases Login y Register. Permisos agregado a DB y nuevos mensajes en messages.properties

--- src/main/resources/database/init_db.sql
+++ src/main/resources/database/init_db.sql
@@ -3,5 +3,9 @@ insert into ESTADOS(ID,VALOR)values (1,'PAGADO');
 insert into ESTADOS(ID,VALOR)values (2,'ENVIADO');
 insert into ESTADOS(ID,VALOR)values (3,'ENTREGADO');
 insert into ESTADOS(ID,VALOR)values (4,'CANCELADO');
+
 insert into METODOS_PAGOS(ID,VALOR)values (0,'DEBITO');
 insert into METODOS_PAGOS(ID,VALOR)values (1,'MERCADO_PAGO');
+
+insert into PERMISOS(ID,NOMBRE)values (0,'ADMINISTRADOR');
+insert into PERMISOS(ID,NOMBRE)values (1,'COMPRADOR');
